64d348b0310be8dfde98549fe7b0eaf6f30dd853
Wael Yasmina <Waelyasmina@outlook.com>
2022-09-21 18:37:18 +0100

    Project files

---
+++ src/js/scripts.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Sets the color of the background
+renderer.setClearColor(0xFEFEFE);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+// Sets orbit control to move the camera around
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+// Camera positioning
+camera.position.set(6, 8, 14);
+orbit.update();
+
+// Sets a 12 by 12 gird helper
+const gridHelper = new THREE.GridHelper(12, 12);
+scene.add(gridHelper);
+
+// Sets the x, y, and z axes with each having a length of 4
+const axesHelper = new THREE.AxesHelper(4);
+scene.add(axesHelper);
+
+function animate() {
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
